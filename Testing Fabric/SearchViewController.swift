4e6cf010d00674bebf9719eca099252229235a85
nimaemrani <nimaemrani30@gmail.com>
2016-02-08 17:41:13 -0800

    Added a VC

---
+++ Testing Fabric/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  Testing Fabric
+//
+//  Created by Nima Emrani on 2/8/16.
+//  Copyright © 2016 Nima Emrani. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    
+    
+}
